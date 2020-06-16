4c4fc44bc74d737ac1af8631263cbcea68201d3d
tariksitki <tariksitki0119@gmail.com>
2020-06-16 22:57:12 +0200

    Js Exercise 62

--- main.js
+++ main.js
@@ -41,3 +41,61 @@ function without_first_end(str) {
           console.log(concatenate("A","B"));
 
           console.log(concatenate("AA","BB"));
+
+
+
+
+
+
+
+          // Exercise 62    
+
+
+              function right_three(str) {
+
+                if (str.length > 1) 
+                
+                  {
+                      return str.slice(-3) + str.slice(0, -3);  // Notiz: Burada -3 dedigi sey, 0 ve 3 . eleman dahil;  -3 dedigi sey de 3. eleman haric ondan sonrakiler basa gelsin + digerleri gelsin
+
+                }
+                      return str;
+                
+              }
+
+              console.log(right_three("Python"));
+
+              console.log(right_three("JavaScript"));
+
+              console.log(right_three("Hi"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              function right_three(str) {
+
+                if (str.length > 1) {
+
+                  return str.slice(-3) + str.slice(0,-3);
+                  
+                }
+
+                  return str;
+              }
+
+                console.log(right_three("Tarik"));
+                
+              
+              
