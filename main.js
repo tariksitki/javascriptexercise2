80aca687239b061ea9f9f73b4fc731403a78941a
tariksitki <tariksitki0119@gmail.com>
2020-06-18 23:32:47 +0200

    JavaScript 67

--- main.js
+++ main.js
@@ -187,6 +187,46 @@ function without_first_end(str) {
 
                                   console.log(city_name("London"));
 
+
+
+
+
+                              // Exercise 67        Create a new string from a given string, removing the first and last characters of the string if the first or last character are "P";
+
+
+
+
+                                  function nop(str) {
+
+                                    let start_pos = 0 ;
+
+                                    let end_pos = str.length;
+
+                                    if (str.length > 0 && str.charAt(0) == "P") 
+                                    
+                                    {
+                                        start_pos = 1;
+                                      
+                                    }
+
+                                    if (str.length > 1 && str.charAt(str.length - 1) == "P")
+                                    
+                                    {
+                                      end_pos--;           // Buradaki -- sondaki harfin okunmamasini komut verir
+                                      
+                                    }
+                                      return str.substring(start_pos, end_pos) ;
+                                  }
+
+                                  
+                                  console.log(nop("PythonP"));
+
+                                  console.log(nop("Python"));
+
+                                  console.log(nop("JavaScript"));
+                                  
+                                  
+
                                   
                                   
                                   
