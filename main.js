f8ccff0f8c3fd58250280c84a54893c3d6ac3c97
tariksitki <tariksitki0119@gmail.com>
2020-06-18 23:19:58 +0200

    JavaScript66

--- main.js
+++ main.js
@@ -162,6 +162,41 @@ function without_first_end(str) {
 
 
 
+                          // Exercise 66    Write a Java Script program to display the city name if the string begins with "Los" or "New" otherwise return blank.
+
+
+
+
+                              function city_name(str) {
+
+                                if (str.length >= 3 && ((str.substring(0,3) == 'Los') || (str.substring(0,3) == "New")))
+
+                                  {    
+                                    return str;
+                                  
+                                }
+                                    return ""  ;
+                              
+
+                              }
+
+
+                                  console.log(city_name("New York"));
+
+                                  console.log(city_name("Los Angeles"));
+
+                                  console.log(city_name("London"));
+
+                                  
+                                  
+                                  
+
+
+
+
+
+
+
            
 
                   
