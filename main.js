d86b662548efce5c9701604014e777915a75b642
tariksitki <tariksitki0119@gmail.com>
2020-06-19 23:13:57 +0200

    JavaScript 70

--- main.js
+++ main.js
@@ -303,6 +303,63 @@ function without_first_end(str) {
 
 
 
+
+
+                                        // Exercise 70    Write a JavaScript program to rotate the elements left of a given array of integers of length 3.
+
+
+                                        //  Verilen 3 ögel arrayin ögelerinin yerini degistiriyoruz. basa ikinci siradaki sona da bastaki gidiyor.
+                                        
+                                        
+
+
+                                        function rotate_elements_left(array) {
+
+                                          return [ array[1] , array[2] , array[0]  ] ; 
+                                          
+                                        }
+
+
+
+                                        console.log(rotate_elements_left([3, 4, 5]));
+
+                                        console.log(rotate_elements_left([0, -1, 2]));
+
+                                        console.log(rotate_elements_left([7,6,5]));
+
+
+                                        
+                                        // Yukaridaki formulün aynisi sadece elemanlari topluyoruz
+                                        
+                                        function sum_elements_left(array) {
+
+                                          return  [ array[1] + array[2] + array[0] ] ;
+                                          
+                                        }
+
+
+                                        console.log(sum_elements_left([10, 11, 12]));
+
+
+                                        
+
+                                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                                         
                                         
                                         
