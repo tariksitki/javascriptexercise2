55494d3940a989788187dd8d84d0ef27009fb253
tariksitki <tariksitki0119@gmail.com>
2020-06-19 23:00:40 +0200

    JavaScript 69

--- main.js
+++ main.js
@@ -271,19 +271,47 @@ function without_first_end(str) {
                                     console.log(two_string("JavaScript" , 1));     
                                     
                                     
+
+
+
+
+
+                                    // Exercise 69   Write a JavaScript Program to compute the sum of three elements of given array of integers of length 3.
+
+
+
+
+                                        function sum_three(nums) {
+
+                                          return nums[0] + nums[1] + nums[2];
+                                          
+                                        }
+
+
+                                        console.log(sum_three([10, 32 , 20]));
+
+                                        console.log(sum_three([5, 7, 9]));
+
+                                        console.log(sum_three([0,8, -11]));
+
+
+
+
+
+
+
+
+
+
+                                        
+                                        
+                                        
                                     
                                   
                                   
                                   
 
-                                  
-                                  
-                                  
-
-
-
-
-
+                                         
 
 
            
