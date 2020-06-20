59331ae25d89487ae82d5d59558e0fd925280a0c
tariksitki <tariksitki0119@gmail.com>
2020-06-20 23:59:30 +0200

    JavaScript 72

--- main.js
+++ main.js
@@ -404,6 +404,38 @@
 
 
 
+                                            // Exercise 72      Write a Js program to check whether the first and last elements are equal of a given array 
+
+
+
+                                            function first_last_same(nums) {
+
+                                              var end = nums.length - 1;
+
+                                              if (nums.length >= 1) {
+
+                                                    return nums[0] == nums[end];
+                                                
+                                              }
+                                                    else {return false;}
+                                              
+                                            }
+
+
+                                                console.log(first_last_same([10,20,30]));
+
+                                                console.log(first_last_same([10,20,30,10]));
+
+                                                console.log(first_last_same([20,20,20]));
+                                                
+                                                
+                                                
+
+
+
+
+
+
 
 
 
