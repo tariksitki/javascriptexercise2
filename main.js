e6924995e56f5d1bc5a5b3225d6fd218fb448ee4
tariksitki <tariksitki0119@gmail.com>
2020-06-21 23:53:46 +0200

    Js 74

--- main.js
+++ main.js
@@ -490,7 +490,7 @@
 
 
 
-                                                    // Yukaridi koda alternatif olarak yazdim
+                                                    // Yukaridaki koda alternatif olarak yazdim
 
 
 
@@ -518,6 +518,44 @@
                                                 
                                                 
 
+
+
+
+
+
+
+                                                    // Exercise 74 Write a JavaScript Program to find the larger value 
+
+                                                    // En Yüksek degeri bulmak
+
+
+
+
+                                                    function all_max(nums) {
+
+                                                      var max_val = nums[0] > nums[2] ? nums[0] : nums[2] ;
+
+                                                    nums[0] = max_val ;
+
+                                                    nums[1] = max_val;
+
+                                                    nums[2] = max_val;
+
+
+                                                      return nums;
+                                                      
+                                                    }
+
+                                                    
+                                                      console.log(all_max([20,30,40]));
+
+                                                      console.log(all_max([-7,-9,0]));
+
+                                                      console.log(all_max([12,10,3]));
+                                                      
+                                                      
+                                                      
+                                                      
 
 
 
