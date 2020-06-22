08add7d425bb37b9edb712350d609dd80142bbd5
tariksitki <tariksitki0119@gmail.com>
2020-06-22 23:40:08 +0200

    Js 75

--- main.js
+++ main.js
@@ -562,7 +562,37 @@
 
 
 
-                        
+                                                        // Exercise 75    To create a new array taking the middle elements of the two arrays 
+
+                                                        // Verilen 2 Array in ortasindaki sayilari alarak  yeni element olusturma
+                                                        
+                                                        
+
+
+
+                                                        function middle_elements(a,b) {
+
+                                                            var new_array = [] 
+
+                                                            new_array.push(a[1], b[1] ) ;
+
+                                                                return new_array;
+                                                          
+                                                        }
+
+
+
+
+                                                              console.log(middle_elements([1,2,3,], [1,5,6]));
+
+                                                              console.log(middle_elements([3,3,3], [2,8,0]));
+
+                                                              console.log(middle_elements([4,2,7], [2,4,5]));
+
+
+                                                              
+                                                              
+                                                              
                                                       
 
 
